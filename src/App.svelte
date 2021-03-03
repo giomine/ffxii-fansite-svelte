80540c6d7909f355c9cbec284a0d68fa049aaa18
giomine <giomineo@gmail.com>
2021-03-03 14:12:49 +0000

    Initial commit includes basic pages and links

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { Router, Route } from "svelte-navigator"
+	import Home from "./Home.svelte"
+	import Characters from "./Characters.svelte"
+	import Monsters from "./Monsters.svelte"
+</script>
+
+
+<Router>
+	<main>
+		<Route path="/">
+			<Home />
+		</Route>
+		<Route path="/characters">
+			<Characters />
+		</Route>
+		<Route path="/monsters">
+			<Monsters />
+		</Route>		
+	</main>
+</Router>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+
+<main>
+    <h1>Hello FFXII Fans!</h1>
+	<p>Navigate the site via page links at the top! Kupo!</p>
+</main>
+
+<style>
+    
+</style>
