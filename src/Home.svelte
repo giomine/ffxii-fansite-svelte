a194f5d7dd90b95abaee54152dd2160897d88cf5
giomine <giomineo@gmail.com>
2021-03-10 15:53:21 +0000

    Add prologue to Home

--- src/Home.svelte
+++ src/Home.svelte
@@ -5,7 +5,11 @@
 
 <main>
     <h1>Hello FFXII Fans!</h1>
-	<p>Navigate the site via page links at the top! Kupo!</p>
+    <!-- <p>Navigate the site via page links at the top! Kupo!</p> -->
+    <p class="prologue">This is the story of a world called Ivalice. Two years after the war in which the kingdom of Dalmasca was destroyed by the 
+        invasion of the mighty military nation, the Arcadia Empire I longed for the freedom to run in the sky. The only surviving 
+        princess in her royal family, Ashe, secretly led the PLA to revive her homeland and vowed that revenge on her empire was her 
+        duty. Their wishes and hopes, which they met during the wartime era, connect the fate of people and change the fate of nations.</p>
 </main>
 
 <style>
@@ -13,7 +17,10 @@
         height: 85vh;
         background: rgb(190, 190, 190);
     }
-    p{
-        margin: 0 auto;
+
+    .prologue{
+        width: 40%;
+        margin: 100px auto;
+        line-height: 1.2;
     }
 </style>
