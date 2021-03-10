5de9d63112edeb8316d68390a58fc511eea9f4ba
giomine <giomineo@gmail.com>
2021-03-10 16:15:56 +0000

    Add Home bg image and edit prologue text styles

--- src/Home.svelte
+++ src/Home.svelte
@@ -4,16 +4,27 @@
 
 
 <main>
-    <p class="prologue">This is the story of a world called Ivalice. Two years after the war in which the kingdom of Dalmasca was destroyed by the 
-        invasion of the mighty military nation, the Arcadia Empire I longed for the freedom to run in the sky. The only surviving 
-        princess in her royal family, Ashe, secretly led the PLA to revive her homeland and vowed that revenge on her empire was her 
-        duty. Their wishes and hopes, which they met during the wartime era, connect the fate of people and change the fate of nations.</p>
+    <div class="prologue-container">
+        <p class="prologue">This is the story of a world called Ivalice. Two years after the war in which the kingdom of Dalmasca was destroyed by the 
+            invasion of the mighty military nation, the Arcadia Empire I longed for the freedom to run in the sky. The only surviving 
+            princess in her royal family, Ashe, secretly led the PLA to revive her homeland and vowed that revenge on her empire was her 
+            duty. Their wishes and hopes, which they met during the wartime era, connect the fate of people and change the fate of nations.</p>
+    </div>
 </main>
 
 <style>
     main{
         height: 85vh;
         background: #F6F6F6;
+        background-image: url(/images/final-fantasy-xii-key-art.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+
+    .prologue-container{
+        height: 100%;
+        display: flex;
+        align-items: center;
     }
 
     .prologue{
@@ -21,5 +32,7 @@
         margin: 0 auto;
         padding: 100px;
         line-height: 1.2;
+        color: rgb(0, 0, 0);
+        background: rgba(255, 255, 255, 0.8);
     }
 </style>
