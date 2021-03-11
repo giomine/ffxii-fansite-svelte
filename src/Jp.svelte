4296baab1c13d5d59c3e3154cbd5e457de8dbd9e
giomine <giomineo@gmail.com>
2021-03-11 14:53:04 +0000

    Add coming soon message to Japanese page

---
+++ src/Jp.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+
+
+<main>
+    <div class="main-container">
+        <h2>日本のサイトはもうすぐです。</h2>
+    </div>
+</main>
+
+
+<style>
+    main{
+        background: #F6F6F6;
+    }
+    .main-container{
+        height: 85vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
